bb6c452da3d164be3042a5b4c307cdde2a3a1872
vishal thakare <130039952+vishal1722@users.noreply.github.com>
2025-04-02 20:57:31 +0530

    Update Profile.js

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -4,11 +4,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 
 const initialProfiles = [
-  { id: 1, name: "John Doe", age: 30, job: "Software Engineer", salary: "$80,000",location:"pune", hobbies: "Reading, Traveling", photo: "/imag1.jpeg", description: "Experienced software developer", lat: 37.7749, lng: -122.4194 },
-  { id: 2, name: "Jane Smith", age: 28, job: "Data Scientist", salary: "$90,000",location:"chennai", hobbies: "Hiking, Cooking", photo: "/image2.jpeg", description: "Data enthusiast", lat: 40.7128, lng: -74.006 },
-  { id: 3, name: "Alice Brown", age: 26, job: "UX Designer", salary: "$75,000",location:"kolkata", hobbies: "Sketching, Gaming", photo: "/image3.jpeg", description: "Creative UI/UX designer", lat: 34.0522, lng: -118.2437 },
-  { id: 4, name: "Michael Johnson", age: 35, job: "Product Manager", salary: "$100,000",location:"hydrabad", hobbies: "Cycling, Reading", photo: "/image4.jpeg", description: "Strategic product planner", lat: 51.5074, lng: -0.1278 },
-  { id: 5, name: "Emily Davis", age: 29, job: "AI Researcher", salary: "$95,000",location:"mubai", hobbies: "Chess, Music", photo: "/image5.jpeg", description: "Passionate AI scientist", lat: 48.8566, lng: 2.3522 }
+  { id: 1, name: "John Doe", age: 30, job: "Software Engineer", salary: "$80,000",location:"pune", hobbies: "Reading, Traveling", photo: "/imag1.jpeg", description: "Experienced software developer", lat: 18.5248706, lng: 73.6981531 },
+  { id: 2, name: "Jane Smith", age: 28, job: "Data Scientist", salary: "$90,000",location:"chennai", hobbies: "Hiking, Cooking", photo: "/image2.jpeg", description: "Data enthusiast", lat: 13.0478078, lng: 80.0438585 },
+  { id: 3, name: "Alice Brown", age: 26, job: "UX Designer", salary: "$75,000",location:"kolkata", hobbies: "Sketching, Gaming", photo: "/image3.jpeg", description: "Creative UI/UX designer", lat: 22.5354441, lng: 88.0354893 },
+  { id: 4, name: "Michael Johnson", age: 35, job: "Product Manager", salary: "$100,000",location:"hydrabad", hobbies: "Cycling, Reading", photo: "/image4.jpeg", description: "Strategic product planner", lat: 17.4126086, lng: 78.2432311 },
+  { id: 5, name: "Emily Davis", age: 29, job: "AI Researcher", salary: "$95,000",location:"mumbai", hobbies: "Chess, Music", photo: "/image5.jpeg", description: "Passionate AI scientist", lat: 19.0730413, lng: 72.8695123 }
 ];
 
 
@@ -73,12 +73,13 @@ function ProfileDetails({ profiles }) {
           <p><strong>Salary:</strong> {profile.salary}</p>
           <p><strong>Location:</strong> {profile.location}</p>
           <p><strong>Hobbies:</strong> {profile.hobbies}</p>
+          <p><strong>description:</strong> {profile.description}</p>
         </div>
         <h3 className="text-center mt-4">Location</h3>
         <iframe
           width="100%"
           height="400"
-          src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d199666.5651251294!2d-121.58334177520186!3d38.56165006739519!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x809ac672b28397f9:0x921f6aaa74197fdb!2sSacramento,%20CA,%20USA!5e0!3m2!1sen!2sbd!4v1647608789441!5m2!1sen!2sbd${profile.lat},${profile.lng}`}
+          src={`https://maps.google.com/maps?q=${profile.lat},${profile.lng}&hl=es&z=14&output=embed`}
           allowFullScreen
           loading="lazy"
         ></iframe>
@@ -151,5 +152,4 @@ function App() {
   );
 }
 
-export default App;
 export default App;
